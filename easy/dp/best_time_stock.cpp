d2e8fefc3ecbc02d6211b322a435d9679290aaf0
Abi Kakolla <abhishekkakolla1@gmail.com>
2025-04-02 23:40:44 -0700

    didn't do dps oops, but better sliding window implementation

---
+++ easy/dp/best_time_stock.cpp
@@ -0,0 +1,22 @@
+// NOT DP woops
+
+
+class Solution {
+    public:
+        int maxProfit(vector<int>& prices) {
+            
+            int l = 0;
+            int max = 0;
+            for (int r = 1; r < prices.size(); ++r) {
+                while (prices[r] < prices[l]) {
+                    l++;
+                }
+                max = std::max(max, prices[r] - prices[l]);
+
+            }
+            return max;
+
+
+            
+        }
+    };
