241366ea4b0a9cd09ca46f3d765730296c1f3a07
Abi Kakolla <abhishekkakolla1@gmail.com>
2025-04-12 23:50:09 -0700

    reset main

--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
-class Solution {
-    public:
-        int maxProfit(vector<int>& prices) {
-            
-            int l = 0;
-            int max = 0;
-            for (int r = 1; r < prices.size(); ++r) {
-                while (prices[r] < prices[l]) {
-                    l++;
-                }
-                max = std::max(max, prices[r] - prices[l]);
-
-            }
-            return max;
+#include <iostream>
+#include <vector>
+#include <string>
 
 
-            
-        }
-    };
+using namespace std;
+int main() {
+
+
+
+
+    return -1;
+}
