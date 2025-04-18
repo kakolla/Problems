ecc0912c57e0e183efce96acfcbe3aad1243460c
Abi Kakolla <abhishekkakolla1@gmail.com>
2025-04-18 03:13:50 -0700

    problem

---
+++ medium/math/rand10.cpp
@@ -0,0 +1,14 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+    public:
+        int rand10() {
+            int n = rand7();
+            int n2 = rand7();
+            
+            
+
+        }
+    };
