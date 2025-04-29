73b97f5621647a2f9243ecfbd40795ba4a5cabb6
Abi Kakolla <abhishekkakolla1@gmail.com>
2025-04-29 00:00:37 -0700

    py data strucs

---
+++ misc/study.py
@@ -0,0 +1,74 @@
+
+
+arr = [1,2,3,4]
+arr.append(4)
+
+
+def bin_search(arr, val):
+    l = 0
+    r = len(arr) - 1
+
+    while (l <= r):
+        mid = (l+r)//2
+
+        if (arr[mid] == val):
+            return mid
+        elif (arr[mid] < val):
+            l = mid +1
+        elif (arr[mid] > val):
+            r = mid-1
+    
+    return -1
+
+
+if "__main__" == __name__:
+    # arr
+    arr = []
+
+    # set 
+    milk = set()
+
+    # dict/hashmap
+    milk2 = {}
+
+    # stack
+    st = []
+    st.append(3)
+    st.append(4)
+    st.append(8)
+    st.append(10)
+    st.pop()
+
+
+    # queue
+    from collections import deque
+    q = deque()
+    q.append(1)
+    q.append(2)
+    q.append(3)
+    q.popleft()
+
+    # priority queue
+    import heapq
+    pq = []
+    heapq.heappush(pq, 3)
+    heapq.heappush(pq, 10)
+    heapq.heappush(pq, 1)
+
+    import random
+    arr = [random.randint(0, 10) for x in range(10)]
+    arr.sort()
+    print(arr)
+    to_find = arr[6]  
+    print(to_find)
+    print(bin_search(arr, to_find))
+    
+
+
+
+
+
+
+
+
+    
