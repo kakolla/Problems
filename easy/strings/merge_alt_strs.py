9a7ae2b5cd77097d43009227e34990d5ee726ecc
kakolla <abhishekkakolla1@gmail.com>
2025-06-03 09:10:43 -0400

    better soln py

--- easy/strings/merge_alt_strs.py
+++ easy/strings/merge_alt_strs.py
@@ -1,35 +1,10 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        i = 0
-        j = 0
-        l1 = len(word1)
-        l2 = len(word2)
         result = ""
-        w1done = False
-        w2done = False
-
-        while True:
-            if i >= l1 and j >=l2:
-                w1done = True
-                w2done = True
-                break
-            elif i >= l1:
-                w1done = True
-                break
-            elif j >= l2:
-                w2done = True
-                break
-            result += word1[i]
-            result += word2[j]
-            i += 1
-            j += 1
-
-        if w1done and not w2done:
-            result += word2[j:]
-        elif not w1done and w2done:
-            result += word1[i:] 
+        for a, b in zip(word1, word2):
+            result += a
+            result += b
         
+        result += word1[len(word2):]
+        result += word2[len(word1):]
         return result
-
-        
-        
