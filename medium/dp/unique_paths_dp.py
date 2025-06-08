16b1f015a01da0715dc58cfe23cdba8eaf3ce90e
kakolla <abhishekkakolla1@gmail.com>
2025-06-08 13:16:29 -0400

    what happened

--- medium/dp/unique_paths_dp.py
+++ medium/dp/unique_paths_dp.py
@@ -2,5 +2,20 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[0 for i in range(m)] for j in range(n)]
 
-        print(dp)
+        # rec rel
+        # d(i,j) = d(i-1,j) + d(i,j-1)
+
+        # base cases
+        for i in range(n):
+            dp[i][0] = 1 # only one way to get here
+        
+        for j in range(m):
+            dp[0][j] = 1 # same
+
+        for i in range(1,n):
+            for j in range(1,m):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+            
+        return dp[n-1][m-1]
         
