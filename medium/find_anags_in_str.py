39ee728381c5bd22486ee9c321613c38dad46837
kakolla <abhishekkakolla1@gmail.com>
2025-08-26 12:58:52 -0700

    find anagram but faster but w memory

--- medium/find_anags_in_str.py
+++ medium/find_anags_in_str.py
@@ -1,26 +1,17 @@
 from typing import List
+from collections import Counter
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         ans = []
-        # return start indices of anagrams of p 
 
         l = 0
         r = len(p)
 
-        # sort p
-        p = "".join(sorted(list(p)))
+        # get freq map of p
+        f = Counter(p)
 
         while r <= len(s):
-            cur = s[l:r]
-            cur = "".join(sorted(list(cur))) # sorted version
-            if cur == p:
-                ans.append(l)
-            l += 1
-            r +=1
+
             
 
-        return ans
-
-
-        
