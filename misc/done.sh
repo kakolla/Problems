7e34fda2c08773e432c73965c5088f768d83e327
kakolla <abhishekkakolla1@gmail.com>
2026-06-17 23:38:42 -0700

    create shell to submit

--- misc/done.sh
+++ misc/done.sh
@@ -2,13 +2,28 @@
 set -e
 
 
-git status
-
-git add main.py
-git add main.cpp
-
 # value of first command line arg
 NAME="$1"
 
-git commit -m "done problem - ${NAME}"
-git push
+if [[ -z "$NAME" ]]; then
+	echo "first arg should be <name>"
+	exit 1
+fi
+
+
+if [[ -f main.py && $(wc -l < main.py) -gt 1 ]]; then
+	cat main.py >> misc/${NAME}.py
+elif [[ -f main.cpp && $(wc -l < main.cpp) -gt 1 ]]; then
+	cat main.cpp >> misc/${NAME}.cpp
+fi
+
+git status
+read -p "continue? (y/n)" confirm 
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+	git commit -m "done problem - ${NAME}"
+	git push
+else
+	exit 1
+fi
+
+
